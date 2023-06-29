f8c8663801076eb306309ebf03f2f01bd256f7c4
Pedro Zich <zichpedro22@gmail.com>
2023-06-29 16:16:07 -0300

    feat: find coordinator by any attribute

--- src/services/coordinator_service.py
+++ src/services/coordinator_service.py
@@ -51,6 +51,25 @@ class CoordinatorService:
         :param coordinator_data: The CoordinatorData object used for data storage.
         """
         self.coordinator_data = coordinator_data
+        self.database = self.coordinator_data.load_coordinators()
+    
+    def find_coordinator_by_type(self, attr_type, value):
+        """
+        Find a coordinator in the database based on the specified attribute type and value.
+
+        Args:
+            attr_type (str): The attribute type to be checked.
+            value: The value of the attribute to be matched.
+
+        Returns:
+            The coordinator object if found, None otherwise.
+        """
+        for coordinator in self.database:
+            if getattr(coordinator, attr_type) == value:
+                return coordinator
+
+        return None
+
 
     def verify_prontuario(self, value):
         """
@@ -100,7 +119,7 @@ class CoordinatorService:
         :param prontuario: The prontuario to check for existence.
         :raises ValueError: If a coordinator with the given prontuario already exists.
         """
-        for coordinator in self.coordinator_data.load_coordinators():
+        for coordinator in self.database:
             if prontuario == coordinator.prontuario:
                 raise CoordinatorAlreadyExists(
                     "ERRO: Já há um coordenador com este prontuário!"
@@ -128,3 +147,4 @@ class CoordinatorService:
             coordenador.email,
         )
         self.coordinator_data.add_coordinator(coordinator)
+        self.database.append(coordinator)
