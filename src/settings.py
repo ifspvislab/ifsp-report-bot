c4f9bb5e2e0f296df8a4db2fa26e29c0084d0656
Nicholas <nicholasfp@hotmail.com>
2023-07-04 22:28:46 -0300

    fixing privacy errors

--- src/settings.py
+++ src/settings.py
@@ -16,11 +16,6 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s --- [%(filename)s | %(funcName)s] : %(message)s",
 )
 
-DISCORD_BOT_TOKEN = (
-    "MTExNjA5NDgwMDM5NjAyNTkzOA.GoLh4A.O80rEMBwy3F9hcd_XCbLuQhE6jcKv8vYpSyW3s"
-)
-ADMIN_DISCORD_ID = "485131474597576724"
-
 
 def get_discord_bot_token() -> str:
     """
@@ -31,7 +26,7 @@ def get_discord_bot_token() -> str:
 
     :raises KeyError: If the 'DISCORD_BOT_TOKEN' environment variable does not exist.
     """
-    discord_bot_token = DISCORD_BOT_TOKEN
+    discord_bot_token = os.getenv('DISCORD_BOT_TOKEN')
     if discord_bot_token is None:
         raise KeyError(
             "The requested environment variable 'DISCORD_BOT_TOKEN' does not exist"
